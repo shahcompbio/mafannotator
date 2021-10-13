ea4a8fd69a17291c96d94edb4aaa9089780ab721
Diljot Grewal <diljot.grewal@gmail.com>
2021-10-13 11:07:35 -0700

    removing build files

--- build/lib/mafannotator/MafAnnotator.py
+++
@@ -1,6 +0,0 @@
-import argparse
-from mafannotator.AnnotatorCore import *
-
-def annotate(input, output, token):
-    setoncokbapitoken(token)
-    processalterationevents(input, output, "", "",{}, False, None, None)
--- build/lib/mafannotator/test.py
+++
@@ -1,5 +0,0 @@
-import mafannotator.MafAnnotator as ma
-i = "/juno/work/shah/isabl_data_lake/analyses/17/72/11772/results/somatic/SA505516/SA505516_consensus_somatic.maf"
-o = "t"
-t = "86c693fc-7cac-4032-9c32-45e07f7e0965"
-ma.annotate(i, o, t)
